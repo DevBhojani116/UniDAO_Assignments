b0dbfec00bb705c1595e8dd041734bd89aa4d06b
DevBhojani116 <devbhojani116@gmail.com>
2023-09-16 22:40:35 +0530

    Adding the assignment of week 2

---
+++ Week2/Week2.sol
@@ -0,0 +1,47 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract transferUponDeath
+{
+    address owner = 0x75a9EAfA69CDFaB59EBe677fD5db85D790566758;
+    address newOwner;
+
+    //checks whether the original owner is calling or someone else
+    modifier isOwner() 
+    {
+        require(msg.sender == owner);
+        _;
+    }
+
+
+    //function to determine the address of the owner who will inherit the balance after original owner's death
+    function setNewOwner(address _newOwner) external isOwner
+    {
+        newOwner = _newOwner; 
+    }
+
+    uint lastBlockAccessed;
+
+     //stores the value of the last block accessed by the owner
+    function callBlock() external isOwner
+    {
+        lastBlockAccessed = block.number;
+    }
+
+    //checks whether the owner is dead or alive based on the condition given
+    function checkDeadOrAlive() public
+    {
+        if(lastBlockAccessed<block.number-10)
+        {
+            isDead();
+        }
+    }
+
+    //this is implemented if the owner is dead, it transfers all the balance of the original owner to the new owner
+    function isDead() internal
+    {
+        uint ownerBalance = owner.balance;
+        payable(newOwner).transfer(ownerBalance);
+    }
+
+    receive() external payable {}
+}
