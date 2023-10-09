d673356417ec80f928d9da0039e0d6e52a9f6eaf
DevBhojani116 <devbhojani116@gmail.com>
2023-10-09 22:47:47 +0530

    modified the solidity code to change image

--- Week 4/my-nft/contracts/MyNFT.sol
+++ Week 4/my-nft/contracts/MyNFT.sol
@@ -9,19 +9,26 @@ contract MyNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    uint256 public max = 3000;
+    uint256 public count = 0;
+
     constructor() ERC721("MyNFT", "NFT") {}
 
-    function mintNFT(address recipient, string memory tokenURI)
-        public
-        onlyOwner
-        returns (uint256)
+    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256)
     {
+        require(count<=max,"Maximum limit reached");
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
-
+        count++;
         return newItemId;
+    }
+
+    function changeImage(uint256 tokenId, string memory newImageURI) public onlyOwner 
+    {
+        require(_exists(tokenId), "Token does not exist");
+        _setTokenURI(tokenId, newImageURI);
     }
 }
